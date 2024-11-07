f6764f78121be4f8f0e81fc5efff7f0c63856394
陳佳吟 <chen19970221@gmail.com>
2024-11-07 15:59:17 +0800

    修改vue模組載入方式

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import './assets/all.css'
 
-
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import App from './App.js'
 
 createApp(App).mount('#app')
